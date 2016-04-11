ef601d59abe80312222f0fc4e7854fa72f8e5b49
hilrob <hillebrand@i-ways.de>
2016-04-11 14:48:33 +0200

    Licence Texts

--- app/code/Iways/PayPalPlus/Model/Api.php
+++ app/code/Iways/PayPalPlus/Model/Api.php
@@ -1,25 +1,18 @@
 <?php
-namespace Iways\PayPalPlus\Model;
-
-
 /**
- *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com and you will be sent a copy immediately.
  *
- * Created on 02.03.2015
  * Author Robert Hillebrand - hillebrand@i-ways.de - i-ways sales solutions GmbH
  * Copyright i-ways sales solutions GmbH © 2015. All Rights Reserved.
  * License http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- *
  */
+namespace Iways\PayPalPlus\Model;
+
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Encryption\EncryptorInterface;
 use PayPal\Rest\ApiContext;

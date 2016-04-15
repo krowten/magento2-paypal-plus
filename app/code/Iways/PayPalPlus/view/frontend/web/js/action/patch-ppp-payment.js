820fb083226f1e63c418849b39a4109097772714
hilrob <hillebrand@i-ways.de>
2016-04-15 12:36:26 +0200

    Sections

--- app/code/Iways/PayPalPlus/view/frontend/web/js/action/patch-ppp-payment.js
+++ app/code/Iways/PayPalPlus/view/frontend/web/js/action/patch-ppp-payment.js
@@ -56,7 +56,6 @@ define(
                 serviceUrl, JSON.stringify(payload)
             ).done(
                 function () {
-                    customerData.invalidate('checkout-data');
                     customerData.invalidate('cart');
                     ppp.doCheckout();
                 }
